3699bf3ddb5cad9c0d3fdbace399544e1cf46bdb
GabrielBall05 <gabrielb4556@gmail.com>
2024-04-01 23:13:57 -0400

    Project Creation

---
+++ MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe.cpp
@@ -0,0 +1,13 @@
+//========================================================
+//Name: Gabriel Ball
+//Date: 04-01-24
+//Desc: Unbeatable Tic Tac Toe AI using Mini Max Algorithm
+//========================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hi";
+}
