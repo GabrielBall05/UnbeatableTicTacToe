14297d33281c8635f1573be6116526fb9e994cb3
GabrielBall05 <gabrielb4556@gmail.com>
2024-04-02 16:15:42 -0400

    Majority of Game Loop Done

--- MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe.cpp
+++ MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe.cpp
@@ -9,22 +9,25 @@
 using namespace std;
 
 void displayBoard(char board[3][3]);
+void userCoordToArrayCoord(int& r, int& c, string row, string col);
+bool gameOver();
 
 int main()
 {
 	char board[3][3] = {
-		{'-','-','-'},
-		{'-','-','-'},
-		{'-','-','-'}
+		{' ',' ',' '},
+		{' ',' ',' '},
+		{' ',' ',' '}
 	};
 	bool playerTurn = true; //Default to true
 
 	cout << "This is the empty Tic-Tac-Toe Board." << endl;
-	cout << "   |   |   " << endl
-		 << "---|---|---" << endl
-		 << "   |   |   " << endl
-		 << "---|---|---" << endl
-		 << "   |   |   " << endl;
+	cout << "   1   2   3 " << endl
+		 << "A    |   |   " << endl
+		 << "  ---|---|---" << endl
+		 << "B    |   |   " << endl
+		 << "  ---|---|---" << endl
+		 << "C    |   |   " << endl << endl << endl;
 	cout << "Since I'm just so confident, you get to choose who goes first. Here are your options: " << endl;
 	cout << "   Type '0' to go first" << endl;
 	cout << "   Type '1' for the AI to go first" << endl; 
@@ -95,20 +98,119 @@ int main()
 			}
 		}
 	}
+	//HOW TO PLAY
+	cout << "Here's how this will go. You will be X, the AI will be O." << endl;
+	cout << "When it is your turn, you will be prompted to input a row." << endl;
+	cout << "After you give the row, you will be asked for a column." << endl;
+	cout << "If that space is empty, your X will be placed there." << endl;
+	cout << "If that space is occupied, you will be asked for a row & column again." << endl;
+	cout << "Enter anything to start the game: " << endl;
+	string cont;
+	cin >> cont;
+	system("CLS");
+
+	displayBoard(board);
 
 	//GAME LOOP
+	while (!gameOver())
+	{
+		int r;
+		int c;
+		if (playerTurn)
+		{
+			cout << "It is your turn. You will now be asked for a row, then a column." << endl;
+			bool validSpot = false;
+			string row;
+			string col;
+			while (!validSpot)
+			{
+				//ROW
+				cout << "Row: ";
+				cin >> row;
+				while (row != "A" && row != "a" && row != "B" && row != "b" && row != "C" && row != "c")
+				{
+					cout << "Invalid Input, please enter A, B or C: ";
+					cin >> row;
+				}
+				//COLUMN
+				cout << "Column: ";
+				cin >> col;
+				while (col != "1" && col != "2" && col != "3")
+				{
+					cout << "Invalid Input, please enter 1, 2 or 3: ";
+					cin >> col;
+				}
+
+				userCoordToArrayCoord(r, c, row, col);
+				if (board[r][c] == 'O' || board[r][c] == 'X')
+				{
+					cout << row << col << " is already marked with an " << board[r][c] << ". Choose another spot." << endl;
+				}
+				else
+				{
+					validSpot = true;
+				}
+			}
+			cout << "Placing your X at " << row << col << endl;
+			board[r][c] = 'X';
+			playerTurn = false;
+		}
+		else
+		{
+			cout << "It is the AI's turn now" << endl;
+			cout << "AI placed it at (r,c)" << endl;
+			playerTurn = true;
+		}
+		system("CLS");
+		displayBoard(board);
+	}
 
 	cout << endl << endl;
-	displayBoard(board);
+}
+
+void userCoordToArrayCoord(int& r, int& c, string row, string col)
+{
+	//SET ROW INT
+	if (row == "A" || row == "a")
+	{
+		r = 0;
+	}
+	else if (row == "B" || row == "b")
+	{
+		r = 1;
+	}
+	else if (row == "C" || row == "c")
+	{
+		r = 2;
+	}
+	//SET COL INT
+	if (col == "1")
+	{
+		c = 0;
+	}
+	else if (col == "2")
+	{
+		c = 1;
+	}
+	else if (col == "3")
+	{
+		c = 2;
+	}
 }
 
 void displayBoard(char board[3][3])
 {
-	cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl
-		<< "-----------" << endl
-		<< " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl
-		<< "-----------" << endl
-		<< " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+	cout << "   1   2   3" << endl
+		 << "A  " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl
+		 << "  -----------" << endl
+		 << "B  " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl
+		 << "  -----------" << endl
+		 << "C  " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl << endl;
+}
+
+bool gameOver()
+{
+	return false;
 }
 
 
