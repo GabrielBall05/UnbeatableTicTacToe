592eeb30a7dc82158572ae5d118043ef70a0f2ba
GabrielBall05 <gabrielb4556@gmail.com>
2024-04-19 18:12:19 -0400

    better code

--- MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe.cpp
+++ MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe.cpp
@@ -7,13 +7,17 @@
 #include <iostream>
 #include <random> 
 #include <windows.h>
+#include <cctype>
+#include <string>
 using namespace std;
 
+int miniMax();
 void displayBoard(char board[3][3]);
+void whoGoesFirstAndRules(bool& playerTurn);
 void userCoordToArrayCoord(int& r, int& c, string row, string col);
+void arrayCoordToUserCoord(string& row, string& col, int r, int c);
 bool gameOver(char board[3][3], string& whoWon);
 void pickRandomEmptySpot(char board[3][3], int& r, int& c);
-void arrayCoordToUserCoord(string& row, string& col, int r, int c);
 
 int main()
 {
@@ -23,94 +27,17 @@ int main()
 		{' ',' ',' '}
 	};
 	bool playerTurn = true; //Default to true
+	//Add a bool draw instead of the whoWon since player never wins anyway
+
 
 	cout << "This is what the Tic-Tac-Toe Board looks like." << endl;
 	displayBoard(board);
-
-	cout << "Since I'm just so confident, you get to choose who goes first. Here are your options: " << endl;
-	cout << "   Type 'M' to go first" << endl;
-	cout << "   Type 'A' for the AI to go first" << endl; 
-	cout << "   Type 'C' for a coin toss" << endl;
-	cout << "Your decision: ";
-
-	string ans;
-	cin >> ans;
-
-	while (ans != "M" && ans != "A" && ans != "C" && ans != "m" && ans != "a" && ans != "c")
-	{
-		cout << "Invalid Response. Please type M, A, or C: ";
-		cin >> ans;
-	}
-
-	if (ans == "A" || ans == "a")
-	{
-		playerTurn = false;
-	}
-	else if (ans == "C" || ans == "c")
-	{
-		//Perform Coin Toss
-		cout << "The coin is in the air! Type 'H' for heads or 'T' for tails: ";
-		string coinDecision;
-		cin >> coinDecision;
-
-		while (coinDecision != "H" && coinDecision != "h" && coinDecision != "T" && coinDecision != "t")
-		{
-			cout << "Invalid Response. Please type H or T: ";
-			cin >> coinDecision;
-		}
-
-		//Random number stuff idektbh
-		random_device rand;
-		mt19937 gen(rand());
-		uniform_int_distribution<>dis(1, 2);
-		int randomNumber = dis(gen);
-
-		if (randomNumber == 1)
-		{
-			cout << "The coin landed on Heads!" << endl;
-			if (coinDecision == "T" || coinDecision == "t")
-			{
-				playerTurn = false;
-			}
-		}
-		else if (randomNumber == 2)
-		{
-			cout << "The coin landed on Tails!" << endl;
-			if (coinDecision == "H" || coinDecision == "h")
-			{
-				playerTurn = false;
-			}
-		}
-		Sleep(2000);
-	}
-
-	system("CLS");
-	if (playerTurn)
-	{
-		cout << "Ok, you will be going first!" << endl;
-	}
-	else
-	{
-		cout << "Ok, the AI will be going first!" << endl;
-	}
-	//Sleep(1500);
-
-	//HOW TO PLAY
-	cout << endl << "==============================HOW TO PLAY==============================" << endl;
-	cout << "Here's how this will go. You will be X, the AI will be O." << endl; //Sleep(2000);
-	cout << "When it is your turn, you will be prompted to input a column." << endl; //Sleep(2000);
-	cout << "After you give the row, you will be asked for a row." << endl; //Sleep(2000);
-	cout << "If that space is empty, your X will be placed there." << endl; //Sleep(2000);
-	cout << "If that space is occupied, you will be asked for a column & row again." << endl << endl; //Sleep(2000);
-	cout << "Enter anything to start the game: ";
-	string cont;
-	cin >> cont;
-	system("CLS");
-
+	whoGoesFirstAndRules(playerTurn);
 	displayBoard(board);
 
+
 	//GAME LOOP
-	string whoWon;
+	string whoWon; //Replace whoWon later with bool draw
 	while (!gameOver(board, whoWon))
 	{
 		string row;
@@ -166,21 +93,24 @@ int main()
 		cout << "The AI placed its 'O' at " << col << row << endl << endl;
 		displayBoard(board);
 	}
+	//GAME LOOP ^^^
+	//Clear screen and display who won (and the finished board state)
 	system("CLS");
 	displayBoard(board);
-	cout << whoWon << " won the game!" << endl;
+	//Realistically, whoWon should be a boolean (bool draw) since it's impossible for the player to win
+	if (whoWon == "DRAW")
+	{
+		cout << "It was a draw! Nobody wins!" << endl;
+	}
+	else
+		cout << whoWon << " won the game!" << endl;
 
 	cout << endl << endl;
 }
 
-void displayBoard(char board[3][3])
+int miniMax()
 {
-	cout << "   A   B   C" << endl
-		<< "1  " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl
-		<< "  -----------" << endl
-		<< "2  " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl
-		<< "  -----------" << endl
-		<< "3  " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl << endl;
+
 }
 
 bool gameOver(char board[3][3], string& whoWon)
@@ -290,75 +220,136 @@ void pickRandomEmptySpot(char board[3][3], int& r, int& c)
 
 void userCoordToArrayCoord(int& r, int& c, string row, string col)
 {
-	//SET ROW INT
-	if (row == "1")
+	r = (row[0] - '0') - 1;
+	if (col == "A" || col == "B" || col == "C")
 	{
-		r = 0;
+		c = col[0] - 65;
 	}
-	else if (row == "2")
+	else if (col == "a" || col == "b" || col == "c")
 	{
-		r = 1;
-	}
-	else if (row == "3")
-	{
-		r = 2;
-	}
-	//SET COL INT
-	if (col == "A" || col == "a")
-	{
-		c = 0;
-	}
-	else if (col == "B" || col == "b")
-	{
-		c = 1;
-	}
-	else if (col == "C" || col == "c")
-	{
-		c = 2;
+		c = col[0] - 97;
 	}
 }
 
 void arrayCoordToUserCoord(string& row, string& col, int r, int c)
 {
-	//SET ROW STRING
-	if (r == 0)
-	{
-		row = "1";
-	}
-	else if (r == 1)
-	{
-		row = "2";
-	}
-	else if (r == 2)
-	{
-		row = "3";
-	}
-	//SET COl STRING
-	if (c == 0)
-	{
-		col = "A";
-	}
-	else if (c == 1)
-	{
-		col = "B";
-	}
-	else if (c == 2)
-	{
-		col = "C";
-	}
+	r++;
+	row = to_string(r);
+
+	c += 65;
+	col = char(c);
 }
 
+void displayBoard(char board[3][3])
+{
+	cout << "   A   B   C" << endl
+		 << "1  " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl
+		 << "  -----------" << endl
+		 << "2  " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl
+		 << "  -----------" << endl
+		 << "3  " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl << endl;
 
-//cout << "         1         2         3    "
-//	   << "                                  " << endl
-//     << "              |         |         " << endl
-//	   << " A       " << board[0][0] << "    |    " << board[0][1] << "    |    " << board[0][2] << "    " << endl
-//	   << "              |         |         " << endl
-//	   << "     -----------------------------" << endl
-//	   << "              |         |         " << endl
-//	   << " B       " << board[1][0] << "    |    " << board[1][1] << "    |    " << board[1][2] << "    " << endl
-//	   << "              |         |         " << endl
-//	   << "     -----------------------------" << endl
-//	   << "              |         |         " << endl
-//	   << " C       " << board[2][0] << "    |    " << board[2][1] << "    |    " << board[2][2] << "    " << endl
-//	   << "              |         |         " << endl;
+	//cout << "         1         2         3    "
+	//	   << "                                  " << endl
+	//     << "              |         |         " << endl
+	//	   << " A       " << board[0][0] << "    |    " << board[0][1] << "    |    " << board[0][2] << "    " << endl
+	//	   << "              |         |         " << endl
+	//	   << "     -----------------------------" << endl
+	//	   << "              |         |         " << endl
+	//	   << " B       " << board[1][0] << "    |    " << board[1][1] << "    |    " << board[1][2] << "    " << endl
+	//	   << "              |         |         " << endl
+	//	   << "     -----------------------------" << endl
+	//	   << "              |         |         " << endl
+	//	   << " C       " << board[2][0] << "    |    " << board[2][1] << "    |    " << board[2][2] << "    " << endl
+	//	   << "              |         |         " << endl;
+}
+
+void whoGoesFirstAndRules(bool& playerTurn)
+{
+	cout << "Since I'm just so confident, you get to choose who goes first. Here are your options: " << endl;
+	cout << "   Type 'M' to go first" << endl;
+	cout << "   Type 'A' for the AI to go first" << endl;
+	cout << "   Type 'C' for a coin toss" << endl;
+	cout << "Your decision: ";
+	string ans;
+	cin >> ans;
+
+	while (ans != "M" && ans != "A" && ans != "C" && ans != "m" && ans != "a" && ans != "c")
+	{
+		cout << "Invalid Response. Please type M, A, or C: ";
+		cin >> ans;
+	}
+
+	if (ans == "A" || ans == "a")
+	{
+		playerTurn = false;
+	}
+	else if (ans == "C" || ans == "c")
+	{
+		//Perform Coin Toss
+		cout << "The coin is in the air! Type 'H' for heads or 'T' for tails: ";
+		string coinDecision;
+		cin >> coinDecision;
+
+		while (coinDecision != "H" && coinDecision != "h" && coinDecision != "T" && coinDecision != "t")
+		{
+			cout << "Invalid Response. Please type H or T: ";
+			cin >> coinDecision;
+		}
+
+		//Random number stuff idektbh
+		random_device rand;
+		mt19937 gen(rand());
+		uniform_int_distribution<>dis(1, 2);
+		int randomNumber = dis(gen);
+
+		if (randomNumber == 1)
+		{
+			cout << "The coin landed on Heads!" << endl;
+			if (coinDecision == "T" || coinDecision == "t")
+			{
+				Sleep(1000);
+				cout << "You picked tails, so you lost the coin toss." << endl;
+				playerTurn = false;
+			}
+			else
+				cout << "You won the coin toss!" << endl;
+		}
+		else
+		{
+			cout << "The coin landed on Tails!" << endl;
+			if (coinDecision == "H" || coinDecision == "h")
+			{
+				Sleep(1000);
+				cout << "You picked heads, so you lost the coin toss." << endl;
+				playerTurn = false;
+			}
+			else
+				cout << "You won the coin toss!" << endl;
+		}
+		Sleep(2000);
+	}
+
+	//system("CLS");
+	if (playerTurn)
+	{
+		cout << "Ok, you will be going first." << endl;
+	}
+	else
+	{
+		cout << "Ok, the AI will be going first." << endl;
+	}
+	//Sleep(1500);
+
+	//HOW TO PLAY
+	cout << endl << "==============================HOW TO PLAY==============================" << endl;
+	cout << "Here's how this will go. You will be X, the AI will be O." << endl; //Sleep(2000);
+	cout << "When it is your turn, you will be prompted to input a column." << endl; //Sleep(2000);
+	cout << "After you give the row, you will be asked for a row." << endl; //Sleep(2000);
+	cout << "If that space is empty, your X will be placed there." << endl; //Sleep(2000);
+	cout << "If that space is occupied, you will be asked for a column & row again." << endl << endl; //Sleep(2000);
+	cout << "Enter anything to start the game: ";
+	string cont;
+	cin >> cont;
+	system("CLS");
+}
