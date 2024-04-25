c6de25a9d4ea269f9ae776c9cd34050c3d54c5cf
GabrielBall05 <gabrielb4556@gmail.com>
2024-04-25 18:29:24 -0400

    Finished Game

--- MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe.cpp
+++ MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe/MiniMaxAlg-TicTacToe.cpp
@@ -15,10 +15,9 @@ int miniMax(char board[3][3], int depth, bool maximizing, int& whoWon);
 void miniMaxStart(char board[3][3], int& bestMoveR, int& bestMoveC, int& whoWon);
 void displayBoard(char board[3][3]);
 void whoGoesFirstAndRules(bool& playerTurn);
-void userCoordToArrayCoord(int& r, int& c, string row, string col);
+void userCoordToArrayCoord(int& r, int& c, char row, char col);
 void arrayCoordToUserCoord(string& row, string& col, int r, int c);
 bool gameOver(char board[3][3], int& whoWon);
-void pickRandomEmptySpot(char board[3][3], int& r, int& c);
 
 int main()
 {
@@ -29,49 +28,46 @@ int main()
 	};
 	bool playerTurn = true; //Default to true
 
-
 	cout << "This is what the Tic-Tac-Toe Board looks like." << endl;
 	displayBoard(board);
+	//This function is really just so that not everything is piled in main
 	whoGoesFirstAndRules(playerTurn);
 	displayBoard(board);
 
-
 	//GAME LOOP
 	int whoWon = 0;
+	int i = 0;
 	while (!gameOver(board, whoWon))
 	{
-		string row;
-		string col;
 		int r;
 		int c;
 		if (playerTurn)
 		{
-			cout << "It is your turn. Enter a column, then enter a row." << endl;
+			//cout << "It is your turn. Enter a column, then enter a row." << endl;
 			bool validSpot = false;
 
 			while (!validSpot)
 			{
-				//COLUMN
-				cout << "Column: ";
-				cin >> col;
-				while (col != "A" && col != "a" && col != "B" && col != "b" && col != "C" && col != "c")
-				{
-					cout << "Invalid Input, please enter A, B or C: ";
-					cin >> col;
-				}
-				//ROW
-				cout << "Row: ";
-				cin >> row;
-				while (row != "1" && row != "2" && row != "3")
-				{
-					cout << "Invalid Input, please enter 1, 2 or 3: ";
-					cin >> row;
-				}
+				if (i == 0 && !validSpot)
+					cout << "The Game Has Begun." << endl << endl;
+				string coord;
+				cout << "Enter a coordinate (ex: B2 for middle): ";
+				cin >> coord;
+				coord[0] = toupper(coord[0]);
 
-				userCoordToArrayCoord(r, c, row, col);
+				while (coord != "A1" && coord != "A2" && coord != "A3" && coord != "B1" && coord != "B2" && coord != "B3" && coord != "C1" && coord != "C2" && coord != "C3")
+				{
+					cout << "Invalid Input, please enter a valid coordinate: ";
+					cin >> coord;
+					coord[0] = toupper(coord[0]);
+				}
+				r = coord[1];
+				c = coord[0];
+
+				userCoordToArrayCoord(r, c, coord[1], coord[0]);
 				if (board[r][c] != ' ')
 				{
-					cout << col << row << " is already marked with an " << board[r][c] << ". Choose another spot." << endl;
+					cout << coord[0] << coord[1] << " is already marked with an " << board[r][c] << ". Choose another spot." << endl;
 				}
 				else
 				{
@@ -80,12 +76,15 @@ int main()
 			}
 			board[r][c] = 'X';
 			playerTurn = false;
+			system("CLS");
 		}
 		else
 		{
-			//pickRandomEmptySpot(board, r, c);
+			if (i != 0)
+				Sleep(1000);
 			int bestMoveR = 999;
 			int bestMoveC = 999;
+			//Make copy board
 			char boardCopy[3][3];
 			for (int r = 0; r < 3; r++)
 			{
@@ -96,21 +95,24 @@ int main()
 			}
 			miniMaxStart(boardCopy, bestMoveR, bestMoveC, whoWon);
 			board[bestMoveR][bestMoveC] = 'O';
-			//board[r][c] = 'O';
 			playerTurn = true;
-			//arrayCoordToUserCoord(row, col, bestMoveR, bestMoveC);
-			//cout << "The AI placed its 'O' at " << col << row << endl << endl;
+			r = bestMoveR;
+			c = bestMoveC;
+			system("CLS");
 		}
-		system("CLS");
-		//arrayCoordToUserCoord(row, col, bestMoveR, bestMoveC);
-		//cout << "The AI placed its 'O' at " << col << row << endl << endl;
+		i++;
 		displayBoard(board);
+		string row;
+		string col;
+		arrayCoordToUserCoord(row, col, r, c);
+		if (playerTurn)
+			cout << "The AI placed its 'O' at " << col << row << endl << endl;
 	}
 	//GAME LOOP ^^^
 	//Clear screen and display who won (and the finished board state)
 	system("CLS");
 	displayBoard(board);
-	gameOver(board, whoWon);
+
 	if (whoWon == 0)
 	{
 		cout << "It was a draw! Nobody wins!" << endl;
@@ -119,7 +121,7 @@ int main()
 	{
 		cout << "The AI Wins!" << endl;
 	}
-	else if (whoWon == -1)
+	else if (whoWon == -1) //This will never happen, but it's here just in case
 	{
 		cout << "You Win!" << endl;
 	}
@@ -136,7 +138,7 @@ void miniMaxStart(char boardCopy[3][3], int& bestMoveR, int& bestMoveC, int& who
 	{
 		for (int c = 0; c < 3 && !foundBestMove; c++)
 		{
-			//If spot is open, make hypothetical move
+			//If spot is open, make move
 			if (boardCopy[r][c] == ' ')
 			{
 				boardCopy[r][c] = 'O';
@@ -149,7 +151,7 @@ void miniMaxStart(char boardCopy[3][3], int& bestMoveR, int& bestMoveC, int& who
 					//Stop loops
 					foundBestMove = true;
 				}
-				boardCopy[r][c] = ' '; //Reset... right?
+				boardCopy[r][c] = ' '; //Reset piece
 			}
 		}
 	}
@@ -157,6 +159,7 @@ void miniMaxStart(char boardCopy[3][3], int& bestMoveR, int& bestMoveC, int& who
 
 int miniMax(char boardCopy[3][3], int depth, bool maximizing, int& whoWon)
 {
+	//Base case is a board end state (someone won or draw)
 	if (gameOver(boardCopy, whoWon))
 	{
 		return whoWon;
@@ -173,15 +176,16 @@ int miniMax(char boardCopy[3][3], int depth, bool maximizing, int& whoWon)
 			{
 				if (boardCopy[r][c] == ' ')
 				{
-					//Make hypothetical move
+					//Make move
 					boardCopy[r][c] = 'O';
-					//Pass hypothetical board
+					//Pass updated board
 					int score = miniMax(boardCopy, depth + 1, false, whoWon);
+					//If it's better for the AI
 					if (score > bestScore)
 					{
 						bestScore = score;
 					}
-					boardCopy[r][c] = ' '; //Reset... right?
+					boardCopy[r][c] = ' '; //Reset piece
 				}
 			}
 		}
@@ -198,15 +202,16 @@ int miniMax(char boardCopy[3][3], int depth, bool maximizing, int& whoWon)
 			{
 				if (boardCopy[r][c] == ' ')
 				{
-					//Make hypothetical move
+					//Make move
 					boardCopy[r][c] = 'X';
-					//Pass hypothetical board
+					//Pass updated board
 					int score = miniMax(boardCopy, depth + 1, true, whoWon);
+					//If it's better for the player
 					if (score < bestScore)
 					{
 						bestScore = score;
 					}
-					boardCopy[r][c] = ' '; //Reset... right?
+					boardCopy[r][c] = ' '; //Reset piece
 				}
 			}
 		}
@@ -225,11 +230,11 @@ bool gameOver(char boardCopy[3][3], int& whoWon)
 			if (boardCopy[r][0] != ' ')
 			{
 				gameOver = true;
-				if (boardCopy[r][0] == 'X')
+				if (boardCopy[r][0] == 'X') //If player's piece is in the winner's row
 				{
 					whoWon = -1;
 				}
-				else if (boardCopy[r][0] == 'O')
+				else if (boardCopy[r][0] == 'O') //If AI's piece is in the winner's row
 				{
 					whoWon = 1;
 				}
@@ -244,11 +249,11 @@ bool gameOver(char boardCopy[3][3], int& whoWon)
 			if (boardCopy[0][c] == 'X' || boardCopy[0][c] == 'O')
 			{
 				gameOver = true;
-				if (boardCopy[0][c] == 'X')
+				if (boardCopy[0][c] == 'X') //If player's piece is in the winner's column
 				{
 					whoWon = -1;
 				}
-				else if (boardCopy[0][c] == 'O')
+				else if (boardCopy[0][c] == 'O') //If AI's piece is in the winner's column
 				{
 					whoWon = 1;
 				}
@@ -260,11 +265,11 @@ bool gameOver(char boardCopy[3][3], int& whoWon)
 	{
 		gameOver = true;
 		//WHO WON
-		if (boardCopy[1][1] == 'X')
+		if (boardCopy[1][1] == 'X') //If player's piece is in the winner's diagonal
 		{
 			whoWon = -1;
 		}
-		else if (boardCopy[1][1] == 'O')
+		else if (boardCopy[1][1] == 'O') //If AI's piece is in the winner's diagonal
 		{
 			whoWon = 1;
 		}
@@ -283,6 +288,7 @@ bool gameOver(char boardCopy[3][3], int& whoWon)
 				}
 				if (i >= 9)
 				{
+					//It was a draw, the game is over and nobody won
 					gameOver = true;
 					whoWon = 0;
 				}
@@ -293,45 +299,21 @@ bool gameOver(char boardCopy[3][3], int& whoWon)
 	return gameOver;
 }
 
-void pickRandomEmptySpot(char board[3][3], int& r, int& c)
+//Takes the user's input and turns it into [0-2][0-2] so I can actually use it
+void userCoordToArrayCoord(int& r, int& c, char row, char col)
 {
-	bool validSpot = false;
-	int randomRow;
-	int randomCol;
-	while (!validSpot)
+	r = (row - '0') - 1;
+	if (col == 'A' || col == 'B' || col == 'C')
 	{
-		random_device rand1;
-		mt19937 gen1(rand1());
-		uniform_int_distribution<>dis1(0, 2);
-		randomRow = dis1(gen1);
-
-		random_device rand2;
-		mt19937 gen2(rand2());
-		uniform_int_distribution<>dis2(0, 2);
-		randomCol = dis2(gen2);
-
-		if (board[randomRow][randomCol] == ' ')
-		{
-			validSpot = true;
-			r = randomRow;
-			c = randomCol;
-		}
+		c = col - 65;
+	}
+	else if (col == 'a' || col == 'b' || col == 'c')
+	{
+		c = col - 97;
 	}
 }
 
-void userCoordToArrayCoord(int& r, int& c, string row, string col)
-{
-	r = (row[0] - '0') - 1;
-	if (col == "A" || col == "B" || col == "C")
-	{
-		c = col[0] - 65;
-	}
-	else if (col == "a" || col == "b" || col == "c")
-	{
-		c = col[0] - 97;
-	}
-}
-
+//Same thing just in reverse so I can display to the user where the AI went
 void arrayCoordToUserCoord(string& row, string& col, int r, int c)
 {
 	r++;
@@ -349,43 +331,29 @@ void displayBoard(char board[3][3])
 		 << "2  " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl
 		 << "  -----------" << endl
 		 << "3  " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl << endl;
-
-	//cout << "         1         2         3    "
-	//	   << "                                  " << endl
-	//     << "              |         |         " << endl
-	//	   << " A       " << board[0][0] << "    |    " << board[0][1] << "    |    " << board[0][2] << "    " << endl
-	//	   << "              |         |         " << endl
-	//	   << "     -----------------------------" << endl
-	//	   << "              |         |         " << endl
-	//	   << " B       " << board[1][0] << "    |    " << board[1][1] << "    |    " << board[1][2] << "    " << endl
-	//	   << "              |         |         " << endl
-	//	   << "     -----------------------------" << endl
-	//	   << "              |         |         " << endl
-	//	   << " C       " << board[2][0] << "    |    " << board[2][1] << "    |    " << board[2][2] << "    " << endl
-	//	   << "              |         |         " << endl;
 }
 
 void whoGoesFirstAndRules(bool& playerTurn)
 {
 	cout << "Since I'm just so confident, you get to choose who goes first. Here are your options: " << endl;
-	cout << "   Type 'M' to go first" << endl;
-	cout << "   Type 'A' for the AI to go first" << endl;
-	cout << "   Type 'C' for a coin toss" << endl;
+	cout << "   Type '1' to go first" << endl;
+	cout << "   Type '2' for the AI to go first" << endl;
+	cout << "   Type '3' for a coin toss" << endl;
 	cout << "Your decision: ";
 	string ans;
 	cin >> ans;
 
-	while (ans != "M" && ans != "A" && ans != "C" && ans != "m" && ans != "a" && ans != "c")
+	while (ans != "1" && ans != "2" && ans != "3")
 	{
-		cout << "Invalid Response. Please type M, A, or C: ";
+		cout << "Invalid Response. Please type 1, 2, or 3: ";
 		cin >> ans;
 	}
 
-	if (ans == "A" || ans == "a")
+	if (ans == "2")
 	{
 		playerTurn = false;
 	}
-	else if (ans == "C" || ans == "c")
+	else if (ans == "3")
 	{
 		//Perform Coin Toss
 		cout << "The coin is in the air! Type 'H' for heads or 'T' for tails: ";
@@ -428,10 +396,10 @@ void whoGoesFirstAndRules(bool& playerTurn)
 			else
 				cout << "You won the coin toss!" << endl;
 		}
-		Sleep(2000);
+		Sleep(3000);
 	}
 
-	//system("CLS");
+	system("CLS");
 	if (playerTurn)
 	{
 		cout << "Ok, you will be going first." << endl;
